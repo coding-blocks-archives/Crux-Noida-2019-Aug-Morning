90f9b834c17427bd9cdc4f005c44d2bdbffa1723
shubham <shubhamrwt02@gmail.com>
2019-12-17 02:23:04 +0530

    Lectures

--- src/com/company/Lec26/EditDistance.java
+++ src/com/company/Lec26/EditDistance.java
@@ -26,7 +26,41 @@ public class EditDistance {
 
            return  1+Math.min(Math.min(Edit(s1.substring(1),s2),Edit(s1,s2.substring(1))),Edit(s1.substring(1),s2.substring(1)));
         }
-
-
     }
+
+    public static int EditItr(String s1,String s2){
+
+        Integer mem[][] = new Integer[s1.length()+1][s2.length()+1];
+
+        for (int i = 0; i <=s1.length() ; i++) {
+
+            for (int j = 0; j <=s2.length() ; j++) {
+
+                if(i==0){
+                    mem[i][j]=j;
+                }
+
+                if(j==0){
+                    mem[i][j]=i;
+                }
+
+                else{
+
+                    if(s1.charAt(i-1)==s2.charAt(j-1)){
+
+                        mem[i][j]= mem[i-1][j-1];
+                    }
+
+                    else{
+
+                        mem[i][j]= 1+ Math.min(Math.min(mem[i-1][j-1],mem[i][j-1]),mem[i-1][j]);
+                    }
+                }
+            }
+        }
+
+        return mem[s1.length()][s2.length()];
+    }
+
+
 }
--- src/com/company/Lec26/LCS.java
+++ src/com/company/Lec26/LCS.java
@@ -5,24 +5,81 @@ public class LCS {
 
     public static void main(String[] args) {
 
-        System.out.println(LCS("abcdef","abdc"));
+        System.out.println(LCS("abcdef", "abdc"));
+
+
+        System.out.println(LcsItrDp("abcdef","abdc"));
+
     }
 
-    public static int LCS(String s1, String s2){
-        if(s1.length()==0 || s2.length()==0){
+    public static int LCS(String s1, String s2) {
+        if (s1.length() == 0 || s2.length() == 0) {
             return 0;
         }
 
-        if(s1.charAt(0)==s2.charAt(0)){
 
-            return 1+LCS(s1.substring(1),s2.substring(1));
+        if (s1.charAt(0) == s2.charAt(0)) {
+
+            return 1 + LCS(s1.substring(1), s2.substring(1));
+        } else {
+
+            return Math.max(LCS(s1.substring(1), s2), LCS(s1, s2.substring(1)));
         }
-
-        else{
-
-            return Math.max(LCS(s1.substring(1),s2),LCS(s1,s2.substring(1)));
-        }
-
-
     }
+
+    public static int LCSDP(String s1, String s2, Integer[][] mem) {
+        if (s1.length() == 0 || s2.length() == 0) {
+            return 0;
+        }
+
+        int ans = 0;
+
+        if (mem[s1.length()][s2.length()] != null) {
+
+            return mem[s1.length()][s2.length()];
+        }
+
+        if (s1.charAt(0) == s2.charAt(0)) {
+
+            ans = 1 + LCS(s1.substring(1), s2.substring(1));
+        } else {
+
+            ans = Math.max(LCS(s1.substring(1), s2), LCS(s1, s2.substring(1)));
+        }
+
+        mem[s1.length()][s2.length()] = ans;
+
+        return mem[s1.length()][s2.length()];
+    }
+
+
+    public static int LcsItrDp(String s1,String s2){
+        Integer mem[][] = new Integer[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 0; i <=s1.length() ; i++) {
+
+            for (int j = 0; j <=s2.length() ; j++) {
+
+                if(i==0 || j==0){
+                    mem[i][j]=0;
+                }
+
+                else{
+
+                    if(s1.charAt(i-1)==s2.charAt(j-1)){
+                        mem[i][j]= 1+mem[i-1][j-1];
+                    }
+
+                    else{
+                        mem[i][j]=Math.max(mem[i-1][j],mem[i][j-1]);
+                    }
+                }
+            }
+        }
+        return mem[s1.length()][s2.length()];
+    }
+
+
+
+
 }
